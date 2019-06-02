7aae3616444cefc57b2a1b61976279405bdf8469
Gayeoon <rkdus1177@naver.com>
2019-06-02 21:51:12 +0900

    병원 스켈레톤 구성

---
+++ app/src/main/java/com/example/rkdus/a2019_epis_tufu4/AlarmActivity.java
@@ -0,0 +1,14 @@
+package com.example.rkdus.a2019_epis_tufu4;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
+
