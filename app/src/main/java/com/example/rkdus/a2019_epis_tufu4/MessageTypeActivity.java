cd321ce925464cc25762a431e21bc11b4c5ddeb2
LeeHaeWon <banner4@cnu.ac.kr>
2019-06-04 18:32:07 +0900

    액티비티 두 개 추가, 이동

---
+++ app/src/main/java/com/example/rkdus/a2019_epis_tufu4/MessageTypeActivity.java
@@ -0,0 +1,13 @@
+package com.example.rkdus.a2019_epis_tufu4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MessageTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_type);
+    }
+}
