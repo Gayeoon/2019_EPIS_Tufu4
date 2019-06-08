b892aca7a8314f45769492dca48999d1f4ae3ba0
Gayeoon <rkdus1177@naver.com>
2019-06-09 01:26:59 +0900

    병원기능추가

--- app/src/main/java/com/example/rkdus/a2019_epis_tufu4/HospitalActivity.java
+++ app/src/main/java/com/example/rkdus/a2019_epis_tufu4/HospitalActivity.java
@@ -1,14 +1,80 @@
 package com.example.rkdus.a2019_epis_tufu4;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class HospitalActivity extends AppCompatActivity {
+    String id;
+    String hos_name;
+
+    TextView name, count;
+    ImageButton status, community, alarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hospital);
+
+        Intent intent = getIntent();
+        id = intent.getStringExtra("id");
+
+        name = (TextView) findViewById(R.id.name);
+        count = (TextView) findViewById(R.id.count);
+
+        status = (ImageButton) findViewById(R.id.status);
+        community = (ImageButton) findViewById(R.id.community);
+        alarm = (ImageButton) findViewById(R.id.alarm);
+
+        hos_name = HospitalName(id);
+
+        name.setText(hos_name);
+        count.setText(Newmessage(id));
+
+        status.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(getApplicationContext(), StatusActivity.class);
+                intent2.putExtra("id", id);
+                startActivity(intent2);
+            }
+        });
+
+        community.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(getApplicationContext(), CommunityActivity.class);
+                startActivity(intent2);
+            }
+        });
+
+        alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(getApplicationContext(), AlarmActivity.class);
+                intent2.putExtra("id", id);
+                startActivity(intent2);
+            }
+        });
+    }
+
+    private int Newmessage(String id) {
+        // To 지원
+        // id 가지고 메세지 몇개 왔는지 검색해주세여!
+        // 혹시 새로운 메세지 중간에 도착하면
+        // count.setText("여기에"); -> 입력하시면 됩니다!
+
+        return 0;
+    }
+
+    private String HospitalName(String id) {
+        // To 지원
+        // id 가지고 hospital_name 좀 검색해주세여!
+
+        return "두부는네모병원";
     }
 }
 
--- app/src/main/java/com/example/rkdus/a2019_epis_tufu4/LoginActivity.java
+++ app/src/main/java/com/example/rkdus/a2019_epis_tufu4/LoginActivity.java
@@ -1,15 +1,47 @@
 package com.example.rkdus.a2019_epis_tufu4;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText id, pw;
+    ImageButton login, join, find;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //v sad
+
+        id = (EditText) findViewById(R.id.id);
+        pw = (EditText) findViewById(R.id.pw);
+
+        login = (ImageButton) findViewById(R.id.login);
+        join = (ImageButton) findViewById(R.id.join);
+        find = (ImageButton) findViewById(R.id.find);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Check(id.getText().toString(), pw.getText().toString())){
+                    Intent intent = new Intent(getApplicationContext(), HospitalActivity.class);
+                    intent.putExtra("id", id.getText().toString());
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean Check(String id, String pw) {
+        // To 지원
+        // DB에 ID/PW 있는지 확인해죠!
+        return false;
     }
 }
 
