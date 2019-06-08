e5676e37dcad708f9c7d3228192e0c62d0d69faf
LeeHaeWon <banner4@cnu.ac.kr>
2019-06-09 03:25:23 +0900

    SearchActivity ListView 작성중 2차 이동을위한

--- app/src/main/java/com/example/rkdus/a2019_epis_tufu4/SearchActivity.java
+++ app/src/main/java/com/example/rkdus/a2019_epis_tufu4/SearchActivity.java
@@ -2,8 +2,10 @@ package com.example.rkdus.a2019_epis_tufu4;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
@@ -12,8 +14,21 @@ import android.widget.Button;
 import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 /*
  * 사용자
@@ -22,14 +37,36 @@ import android.widget.Toast;
  * - 이해원
  */
 public class SearchActivity extends AppCompatActivity {
+    public static final String HOSPITAL_API_URL = "http://211.237.50.150:7080/openapi/sample/xml/Grid_20141225000000000161_1/1/5?";
+    public static final String HOSPITAL_API_KEY = "API_KEY=9e419c9fb4293de4b448d0f553b753baddda8bc74c07e14f64fcb69ae1cbde4e&";
+    /*
+    반려동물 등록대행업체 조회 API DATA 속성정보
+
+    1   주소           ADDR           반려동물 등록대행업체의 사무실주소
+    2   상세주소       DETAIL_ADDR       반려동물 등록대행업체의 사무실의 상세주소
+    3   대표자명       RPRSNTV_NM       반려동물 등록대행업체의 대표자명
+    4   업체명           ENTRPS_NM       반려동물 등록대행업체명
+    5   업체전화번호   ENTRPS_TELNO   반려동물 등록대행업체 전화번호
+
+    API_KEY       STRING(기본)   sample           발급받은 API_KEY
+    TYPE       STRING(기본)   xml               요청파일 타입 xml, json
+    API_URL       STRING(기본)   Grid_000001       OpenAPI 서비스 URL
+    START_INDEX   INTEGER(기본)   1               요청시작위치
+    END_INDEX   INTEGER(기본)   5               요청종료위치
+    RPRSNTV_NM   STRING(필수)   죽전동물병원   업체 명
+     */
+
     boolean isSearchCurrentLocation, isSignUpApp;
     final String switchOnColor = "#0067A3";
     final String switchOffColor = "#000000";
     String searchResult;
+    ArrayList<SearchItemData> searchList = new ArrayList<>();
+    SearchAsyncTask searchAsyncTask;
 
     ImageView ivSearchBtn;
     EditText eSearch;
     TextView searchCurrentLocationSwitch;
+    ListView lvSearchList;
     CheckedTextView ctvIsSignUpApp;
 
     @Override
@@ -40,12 +77,14 @@ public class SearchActivity extends AppCompatActivity {
         // 변수 값 초기화
         isSearchCurrentLocation = false;
         isSignUpApp = false;
+        searchAsyncTask = new SearchAsyncTask();
 
         // 레이아웃 뷰 정의
         ivSearchBtn = (ImageView) findViewById(R.id.searchBtn);
         eSearch = (EditText) findViewById(R.id.searchEditText);
         searchCurrentLocationSwitch = (TextView) findViewById(R.id.isSearchCurrentLocation);
         ctvIsSignUpApp = (CheckedTextView) findViewById(R.id.isSignUpApp);
+        lvSearchList = (ListView) findViewById(R.id.searchListView);
 
         Button button = (Button) findViewById(R.id.temp);
         button.setOnClickListener(new View.OnClickListener() {
@@ -63,7 +102,7 @@ public class SearchActivity extends AppCompatActivity {
                 switch (actionId) {
                     case EditorInfo.IME_ACTION_SEARCH: // 자판에서 검색 모양 아이콘을 누르면
                         Toast.makeText(getApplicationContext(), "검색을 시작합니다.", Toast.LENGTH_LONG).show();
-                        searchResult = eSearch.getText().toString();    // 검색어 별도의 변수에 저장
+                        searchResult = eSearch.getText().toString().trim();    // 검색어 별도의 변수에 저장
                         showSearchList();
                         break;
                     default:
@@ -80,7 +119,7 @@ public class SearchActivity extends AppCompatActivity {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:   // 클릭 시
                         Toast.makeText(getApplicationContext(), "검색 이미지 click", Toast.LENGTH_LONG).show();
-                        searchResult = eSearch.getText().toString();    // 검색어 별도의 변수에 저장
+                        searchResult = eSearch.getText().toString().trim();    // 검색어 별도의 변수에 저장. trim으로 공백 제거
                         showSearchList();
                         break;
                     case MotionEvent.ACTION_CANCEL: // 클릭하지 않은 상태 시
@@ -115,6 +154,7 @@ public class SearchActivity extends AppCompatActivity {
                 switchOnOff();  // 현재 상태에 따라 on / off 변경
             }
         });
+
     }
 
     /*
@@ -138,11 +178,12 @@ public class SearchActivity extends AppCompatActivity {
     조건에 따라 리스트 보여주는 함수
      */
     private void showSearchList() {
-        searchResult = searchResult.trim();
-        if(searchResult.getBytes().length <= 0) { // null(빈 값) 처리
-
-
+        if(TextUtils.isEmpty(searchResult)) { // 공백처리
             Toast.makeText(getApplicationContext(), "값을 입력해주세요.", Toast.LENGTH_LONG).show();
+        }
+        else {
+            String HOSPITAL_API = HOSPITAL_API_URL + HOSPITAL_API_KEY;
+            searchAsyncTask.execute(HOSPITAL_API);
         }
     }
 
@@ -157,4 +198,65 @@ public class SearchActivity extends AppCompatActivity {
 
         }
     }
+
+    /*
+    검색을 위한 백그라운드 작업 + UI Thread 활용하는 AsyncTask
+     */
+    private class SearchAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String APIurl = strings[0];
+            APIurl = APIurl + "searchResult";
+            try {
+                URL url = new URL(APIurl);
+                InputStream is = url.openStream();
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                XmlPullParser parser = factory.newPullParser();
+                parser.setInput(new InputStreamReader(is, "UTF-8"));
+
+                String tag;
+                // 각각 값을 제대로 받았는지 체크하기 위한 boolean
+                boolean isGetHospitalName = false;
+                boolean isGetCeoName = false;
+                boolean isGetPhoneNum = false;
+                int eventType = parser.getEventType();
+
+                // 파싱 시작
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    switch (eventType) {
+                        case XmlPullParser.START_DOCUMENT: // xml의 시작순간
+                            break;
+                        case XmlPullParser.END_DOCUMENT: // xml의 끝순간
+                            break;
+                        case XmlPullParser.START_TAG:
+                    }
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+        }
+    }
+
+    /*
+    어플등록 따지는 함수
+    DB들어가서 체크하기
+     */
+
+    /*
+    어플등록 필터 여부에 따른 Array값 정리
+     */
 }
