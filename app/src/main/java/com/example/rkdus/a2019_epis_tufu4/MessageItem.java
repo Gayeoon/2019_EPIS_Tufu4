d3f1a09a24ef5f4e285860a4fc02c301fb887ae0
Gayeoon <rkdus1177@naver.com>
2019-06-11 04:10:37 +0900

    메세지 리스트뷰 작성

---
+++ app/src/main/java/com/example/rkdus/a2019_epis_tufu4/MessageItem.java
@@ -0,0 +1,17 @@
+package com.example.rkdus.a2019_epis_tufu4;
+
+public class MessageItem {
+    String name;
+
+    public MessageItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/com/example/rkdus/a2019_epis_tufu4/ReservationActivity.java
+++ app/src/main/java/com/example/rkdus/a2019_epis_tufu4/ReservationActivity.java
@@ -1,14 +1,25 @@
 package com.example.rkdus.a2019_epis_tufu4;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
 
 public class ReservationActivity extends AppCompatActivity {
+
+    ImageButton call, check;
+    String tel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
+
+        tel = "tel:01044910778";
+
+        startActivity(new Intent("android.intent.action.DIAL", Uri.parse(tel)));
+
     }
 }
 
